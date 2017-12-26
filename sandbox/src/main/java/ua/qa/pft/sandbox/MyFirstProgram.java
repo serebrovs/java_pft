870690ea20581e8d4a868edbfaa9b0b4a44cfc6e
serebrovs <33724714+serebrovs@users.noreply.github.com>
2017-12-26 18:11:51 +0200

    change code style

--- sandbox/src/main/java/ua/qa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/qa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,7 @@
 package ua.qa.pft.sandbox;
 
 public class MyFirstProgram {
-   public static void main(String[] args) {
-      System.out.println("Hello, World");
-   }
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
 }
